f9cad1b0db1451592ffcc9448758da9551587014
lucasramallo <lucasdebrito.jd@gmail.com>
2024-01-19 23:22:31 -0300

    Creating a @spec to the create_user function

--- lib/controllers/user_controller.ex
+++ lib/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule Controllers.UserController do
 
   alias Schemas.User
 
+  @spec create_user(name :: String.t(), age :: Integer.t(), weight :: Float.t(), height :: Integer.t()) :: User.t()
   def create_user(name, age, weight, height) do
     %User{
       name: name,
